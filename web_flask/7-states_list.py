813159dca7f9e0924b38bb17705c5603074cb6ca
NEAZYIT <touihnazih28@gmail.com>
2023-12-27 07:21:55 -0800

    defining these back_populates attributes in both models, you establish the bidirectional relationship between Place and Amenity

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+
+import os
+import sys
+from flask import Flask, render_template
+
+# Adding the parent directory to the path
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page with a list of states"""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
