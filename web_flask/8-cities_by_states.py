6e11296fd3bc4d6c13454ff3051eb533a375c194
NEAZYIT <touihnazih28@gmail.com>
2023-12-27 14:57:19 -0800

    This commit introduces a Flask web application that renders a page displaying cities organized by states. The script defines routes to handle requests for the '/cities_by_states' endpoint. It fetches data from the storage engine, sorts the states alphabetically, and renders them with associated cities in an HTML template. Additionally, it includes proper teardown handling to close the storage session after each request.

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Starts a Flask web application """
+
+import os
+import sys
+import logging
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+# Adding the parent directory to the path
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states', methods=['GET'])
+def cities_by_states():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+
+    return render_template('8-cities_by_states.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
